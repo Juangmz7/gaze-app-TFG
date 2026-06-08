4dc3880d7b74053530cbaf8356d6dd0e19771527
Juan Gomez <juanjuangmo@gmail.com>
2026-06-08 21:40:52 +0200

    refactor(logs): Move appender package

--- social-service/src/main/java/com/app/socialservice/config/observability/InstallOpenTelemetryAppender.java
+++ social-service/src/main/java/com/app/socialservice/config/observability/InstallOpenTelemetryAppender.java
@@ -1,4 +1,4 @@
-package com.app.socialservice.config;
+package com.app.socialservice.config.observability;
 
 import io.opentelemetry.api.OpenTelemetry;
 import io.opentelemetry.instrumentation.logback.appender.v1_0.OpenTelemetryAppender;
