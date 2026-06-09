125c68b3df12db4d763ac808c24fe3221eb36547
Juan Gomez <juanjuangmo@gmail.com>
2026-06-09 18:41:33 +0200

    refactor(date): Change date to UTC

--- social-service/src/main/java/com/app/socialservice/SocialServiceApplication.java
+++ social-service/src/main/java/com/app/socialservice/SocialServiceApplication.java
@@ -3,10 +3,12 @@ package com.app.socialservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class SocialServiceApplication {
-
     public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
         SpringApplication.run(SocialServiceApplication.class, args);
     }
 
