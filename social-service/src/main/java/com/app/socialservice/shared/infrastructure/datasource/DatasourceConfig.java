c6560747a9d1100618f637dcaafb7298acd20302
Juan Gomez <juanjuangmo@gmail.com>
2026-06-10 20:35:36 +0200

    fix(hikari): Add hikari datasource bean

--- social-service/src/main/java/com/app/socialservice/shared/infrastructure/datasource/DatasourceConfig.java
+++ social-service/src/main/java/com/app/socialservice/shared/infrastructure/datasource/DatasourceConfig.java
@@ -1,6 +1,8 @@
 package com.app.socialservice.shared.infrastructure.datasource;
 
 import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.LazyConnectionDataSourceProxy;
@@ -9,6 +11,14 @@ import javax.sql.DataSource;
 
 @Configuration
 public class DatasourceConfig {
+    @Bean
+    @ConfigurationProperties("spring.datasource.hikari")
+    public HikariDataSource hikariDataSource(DataSourceProperties properties) {
+        return properties.initializeDataSourceBuilder()
+                .type(HikariDataSource.class)
+                .build();
+    }
+
     @Bean
     public DataSource dataSource(HikariDataSource hikari) {
         return new LazyConnectionDataSourceProxy(hikari);
