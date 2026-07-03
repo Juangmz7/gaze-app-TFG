5b7e19b56c996ec4bdba07cfb96208e165a2840e
Juan Gomez <juanjuangmo@gmail.com>
2026-07-03 13:41:01 +0200

    fix(social): Add userId to recommended response DTO

--- social-service/src/main/java/com/app/socialservice/user/application/dto/RecommendedUserResponse.java
+++ social-service/src/main/java/com/app/socialservice/user/application/dto/RecommendedUserResponse.java
@@ -1,6 +1,9 @@
 package com.app.socialservice.user.application.dto;
 
+import java.util.UUID;
+
 public record RecommendedUserResponse(
+        UUID userId,
         String username,
         String description,
         String profilePic,
