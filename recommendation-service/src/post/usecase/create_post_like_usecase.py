8ffc451bd405b2030f3c75869619119fb1eb69a7
Juan Gomez <juanjuangmo@gmail.com>
2026-08-27 16:03:03 +0200

    Fix: Change user embedding calculations in order to prevent invalid operations exception

--- recommendation-service/src/post/usecase/create_post_like_usecase.py
+++ recommendation-service/src/post/usecase/create_post_like_usecase.py
@@ -9,10 +9,9 @@ from pipeline.exceptions.exceptions import (
 )
 from pipeline.model.interaction.decayed_interaction_stats import DecayedInteractionStats
 from pipeline.model.interaction.raw_interaction_stats import RawInteractionStats
-from pipeline.repository import post_features_repository
 from pipeline.repository.post_features_repository import PostFeaturesRepository
-from pipeline.repository.user_creator_features_repository import UserCreatorFeaturesRepository
 from pipeline.repository.post_tag_features_repository import PostTagFeaturesRepository
+from pipeline.repository.user_creator_features_repository import UserCreatorFeaturesRepository
 from pipeline.repository.user_features_repository import UserFeaturesRepository
 from post.command.post_commands import CreatePostLikeCommand
 from shared.helpers import decay
@@ -24,8 +23,8 @@ class CreatePostLikeUsecase:
             user_creator_features_repository: UserCreatorFeaturesRepository,
             post_tag_features_repository: PostTagFeaturesRepository,
             user_features_repository: UserFeaturesRepository,
-            post_features_repository: PostFeaturesRepository
-        ):
+            post_features_repository: PostFeaturesRepository,
+    ):
         self.user_creator_features_repository = user_creator_features_repository
         self.post_tag_features_repository = post_tag_features_repository
         self.user_features_repository = user_features_repository
@@ -37,51 +36,86 @@ class CreatePostLikeUsecase:
         )
         if user_creator_features is None:
             raise UserCreatorFeaturesNotFoundException(command.post_id, command.user_id)
-             
 
         post_features = self.post_features_repository.get_post_features(command.post_id)
         if post_features is None:
             raise PostFeaturesNotFoundException(command.post_id)
 
-        post_tags_features = self.post_tag_features_repository.getPostsTagsFeatures(command.post_id, post_features.tags)
+        post_tags_features = self.post_tag_features_repository.getPostsTagsFeatures(
+            command.user_id,
+            post_features.tags,
+        )
         if post_tags_features is None:
             raise PostTagFeaturesNotFoundException(command.post_id)
 
-        post_raw_interaction_stats = [
+        raw_interaction_stats = [
             tag_features.raw_interaction_stats for tag_features in post_tags_features
-        ].append(user_creator_features.raw_interaction_stats)
-        self._update_raw_interaction_stats(user_creator_features.raw_interaction_stats)
+        ]
+        raw_interaction_stats.append(user_creator_features.raw_interaction_stats)
+        self._update_raw_interaction_stats(raw_interaction_stats)
 
-        post_decayed_interaction_stats = [
+        decayed_interaction_stats = [
             (tag_features.decayed_interaction_stats, tag_features.last_updated_at)
-             for tag_features in post_tags_features
-        ].append(user_creator_features.decayed_interaction_stats, user_creator_features.last_updated_at)
-        self._update_decayed_interaction_stats(user_creator_features.decayed_interaction_stats, user_creator_features.last_updated_at)
+            for tag_features in post_tags_features
+        ]
+        decayed_interaction_stats.append(
+            (
+                user_creator_features.decayed_interaction_stats,
+                user_creator_features.last_updated_at,
+            )
+        )
+        self._update_decayed_interaction_stats(decayed_interaction_stats)
 
         user_creator_features.recalculate_affinity_score()
-        [post_tag_feature.recalculate_affinity_score() for post_tag_feature in post_tags_features]
+        for post_tag_feature in post_tags_features:
+            post_tag_feature.recalculate_affinity_score()
 
-        self._update_user_semantic_embedding(command.user_id, command.post_id)
+        self._update_user_semantic_embedding(
+            command.user_id,
+            post_features.semantic_embedding,
+        )
+
         self.user_creator_features_repository.save(user_creator_features)
         self.post_tag_features_repository.save_all(post_tags_features)
 
-
-    def _update_raw_interaction_stats(interactions_stats: list[RawInteractionStats]):
+    def _update_raw_interaction_stats(
+            self,
+            interactions_stats: list[RawInteractionStats],
+    ) -> None:
         for interaction_stats in interactions_stats:
             interaction_stats.likes += 1
 
+    def _update_decayed_interaction_stats(
+            self,
+            decayed_interactions_stats: list[tuple[DecayedInteractionStats, datetime]],
+    ) -> None:
+        for decayed_interaction_stats, last_updated_at in decayed_interactions_stats:
+            decayed_interaction_stats.likes = (
+                decayed_interaction_stats.likes * decay(last_updated_at) + 1
+            )
 
-    def _update_decayed_interaction_stats(decayed_interactions_stats: list[DecayedInteractionStats, datetime]):
-        for decayed_interaction_stats, last_updated_at in decayed_interaction_stats:
-            decayed_interaction_stats.likes = decayed_interaction_stats.likes * decay(last_updated_at) + 1
-            decayed_interaction_stats.likes *= decay(last_updated_at) + 1
+    def _update_user_semantic_embedding(
+            self,
+            user_id: UUID,
+            post_semantic_embedding: list[float],
+    ) -> None:
+        user_features = self.user_features_repository.get_user_features(user_id)
+        decayed_user_embedding = [
+            value * decay(user_features.last_updated_at)
+            for value in user_features.semantic_embedding
+        ]
+        weighted_post_embedding = [
+            value * POST_LIKE_WEIGHT
+            for value in post_semantic_embedding
+        ]
 
+        user_features.semantic_embedding = [
+            user_value + post_value
+            for user_value, post_value in zip(
+                decayed_user_embedding,
+                weighted_post_embedding,
+                strict=True,
+            )
+        ]
 
-    def _update_user_semantic_embedding(self, user_id: UUID, post_semantic_embedding: list[float] ):
-        user_feature = self.user_features_repository.get_user_features(user_id)
-    
-        user_feature.semantic_embedding = (
-            user_feature.semantic_embedding * decay(user_feature.last_updated_at) + post_semantic_embedding * POST_LIKE_WEIGHT
-        )
-
-        self.user_features_repository.save(user_feature)
+        self.user_features_repository.save(user_features)
